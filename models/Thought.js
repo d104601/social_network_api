bcfea18d0d339ec1066f7c872f23195fe33a554f
Taeyong Lee <d104601@hotmail.com>
2022-07-07 21:30:50 -0400

    models

---
+++ models/Thought.js
@@ -0,0 +1,72 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+function convertDate(val) {
+    return moment(val).format("MMM DD, YYYY [at] hh:mm a");
+}
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId();
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: convertDate(createdAt)
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: convertDate(createdAt)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model("thought", thoughtSchema);
+
+module.export = Thought;
--- models/User.js
+++ models/User.js
@@ -2,12 +2,43 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        username: String,
-        email: String,
-        thoughts: [],
-        friends: []
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Valid email address required"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 );
+
+userSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+})
 
 const User = model('user', userSchema);
 
