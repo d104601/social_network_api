911bba9bb4da1218602be9d4356c14082abd7260
Taeyong Lee <d104601@hotmail.com>
2022-07-07 21:32:49 -0400

    models

--- models/Thought.js
+++ models/Thought.js
@@ -9,7 +9,7 @@ const reactionSchema = new Schema(
     {
         reactionId: {
             type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId();
+            default: () => new Types.ObjectId()
         },
         reactionBody: {
             type: String,
